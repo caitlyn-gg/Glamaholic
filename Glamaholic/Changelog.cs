a208e84fc3328178bed48065d0df23fd2b5fd1c6
Caitlyn <85707485+caitlyn-gg@users.noreply.github.com>
2026-03-27 23:12:37 +0000

    update changelog
    one day I'll remember to do this in the bump commit

--- Glamaholic/Changelog.cs
+++ Glamaholic/Changelog.cs
@@ -19,6 +19,15 @@ namespace Glamaholic {
                 "Dyes on items already in the current plate will no longer be ovewritten and waste a dye",
                 "Plates should now always fully apply and refresh on first try",
             ]),
+            
+            new ChangelogEntry(new Version(1, 14, 0, 0), [
+                "The '(Dye)' suffix is now present in clipboard exports for better interop with other plugins (thanks @SubaruYashiro)",
+                "Added folder selection to plate creation (thanks @firstmagic)",
+                "-'New Plate' button now allows picking a folder to create the plate in",
+                "-Clipboard and EC imports now allow picking a folder to create the plate in",
+                "-TryOn/Examine dropdowns now allow picking a folder to create the plate in",
+                "-You can now move plates into folders via the right-click context menu in the list"
+            ]),
         ];
 
         /// <summary>
